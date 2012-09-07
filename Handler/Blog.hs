52f3659e05be64bbd76bbdbc4ceb9909fe06af29
TANIGUCHI Takaki <takaki@asis.media-as.org>
2012-09-07 10:13:52 +0900

    Day 4 (create a new post)

--- Handler/Blog.hs
+++ Handler/Blog.hs
@@ -1,6 +1,8 @@
 module Handler.Blog where
 
 import Import
+import Data.Monoid
+import Data.Time
 
 import Yesod.Form.Nic (YesodNic, nicHtmlField)
 instance YesodNic App
@@ -11,4 +13,29 @@ getBlogR = do
   defaultLayout $ do
     setTitle "Blog Index"
     $(widgetFile "blog")
+
+postForm :: Maybe Post -> Form Post
+postForm p = renderDivs $ Post 
+  <$> areq textField "Name"  (postName  <$> p)
+  <*> areq textField "Title" (postTitle <$> p)
+  <*> areq nicHtmlField "Content" (postContent <$> p)
+  <*> aformM (liftIO getCurrentTime)
+
+getPostNewR :: Handler RepHtml
+getPostNewR = do
+  (postWidget, enctype) <- generateFormPost $ postForm Nothing
+  defaultLayout $ do
+    $(widgetFile "post_new")
+
+postPostNewR :: Handler RepHtml
+postPostNewR = do
+    ((res,postWidget),enctype) <- runFormPost $ postForm Nothing
+    case res of 
+         FormSuccess post -> do 
+            postId <- runDB $ insert post
+            setMessage $ toHtml $ (postTitle post) <> " created"
+            redirect $ BlogR 
+         _ -> defaultLayout $ do
+                setTitle "Please correct your entry form"
+                $(widgetFile "post_new")
 
